923191c69c5b6ef27d908f003c3d140e437716a3
Viduni <vids2002@students.cs.ubc.ca>
2024-04-07 17:09:20 -0700

    Phase 4: Task 2 (fixed checkstyle errors)

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -33,22 +33,23 @@ public class Event {
      * Gets the description of this event.
      * @return  the description of the event
      */
-    public String getDescription () {
+    public String getDescription() {
         return description;
     }
 
     @Override
     public boolean equals(Object other) {
-        if (other == null)
+        if (other == null) {
             return false;
+        }
 
-        if (other.getClass() != this.getClass())
+        if (other.getClass() != this.getClass()) {
             return false;
+        }
 
         Event otherEvent = (Event) other;
 
-        return (this.dateLogged.equals(otherEvent.dateLogged) &&
-                this.description.equals(otherEvent.description));
+        return (this.dateLogged.equals(otherEvent.dateLogged) && this.description.equals(otherEvent.description));
     }
 
     @Override
